c2248f239b45cadba5adbc0e987769135d31c8ad
Y Williams <ygwilliams1@comcast.net>
2017-12-01 20:32:17 -0500

    Added functionality to server

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -4,8 +4,8 @@
 // console.log(name);
 
 const {MongoClient, ObjectID} = require('mongodb');
-var obj = new ObjectID();
-console.log(obj);
+// var obj = new ObjectID();
+// console.log(obj);
 
 
 MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
