4d7aa640739860ad9d4000bbac827584da1ced6a
Y Williams <ygwilliams1@comcast.net>
2017-12-19 20:57:25 -0500

    Added /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
 
 var app = express();
 
@@ -15,7 +15,7 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-  todo.save().then( (doc) => {
+  todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
@@ -23,10 +23,28 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then( (todos) => {
-    res.send({todos})
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
